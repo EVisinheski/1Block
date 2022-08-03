b15fa911c52ee1b87e94841103f6bf1b93cc2157
EVisinheski <eduardovisinheski@gmail.com>
2022-08-03 18:26:40 +0100

    version #0.0.3

---
+++ root/javascript/stickyNav.js
@@ -0,0 +1,26 @@
+
+
+/*
+Currently not working in future to
+transition nav bar to change colour depending on the section scrolled at
+*/
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         var scrollTop = $(window).scrollTop();
+//         if (scrollTop > 50) {
+//             $('body').addClass('header-fixed');
+//         } else {
+//             $('body').removeClass('header-fixed');
+//         }
+
+//         var topOffset = $('#about').offset().top;
+//         var headerHeight = $('#topnav').height();
+//         var transitionPoint = topOffset - headerHeight;
+//         if (scrollTop > transitionPoint) {
+//             $('#topnav').addClass('alt-header');
+
+//         } else {
+//             $('#topnav').removeClass('alt-header');
+//         }
+//     });
+// });
